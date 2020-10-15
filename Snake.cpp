7d2e218abf9ad28e237b021a9f61d16a3c06f43c
A1357935 <1617419773@qq.comm>
2020-10-15 15:00:33 +0800

    update

--- Snake.cpp
+++ Snake.cpp
@@ -1,6 +1,7 @@
-#include<graphics.h>
 #include<stdio.h>
 #include<conio.h>
+#include<graphics.h>
+#include<time.h>
 
 typedef struct pointxy
 {
@@ -21,6 +22,18 @@ struct myfood
 	int flag = 0;
 	int eatGrade = 0;
 } food;
+
+struct myBoom
+{
+	MYPOINT Boomxy;
+	int flag = 0;
+}boom;
+
+struct myGrass
+{
+	MYPOINT grassxy;
+	int flag = 0;
+}grass;
 
 enum movPosition { right, left, down, up };
 
@@ -47,7 +60,7 @@ int drawSnake()
 {
 	for (int i = 0; i < snake.num; i++)
 	{
-		setlinecolor(WHITE);
+		setlinecolor(BLACK);
 		setfillcolor(GREEN);
 		fillrectangle(snake.xy[i].x, snake.xy[i].y, snake.xy[i].x + 10, snake.xy[i].y + 10);
 	}
@@ -56,7 +69,7 @@ int drawSnake()
 
 int moveSnake()
 {
-	for(int i = snake.num - 1;i>0;i--)
+	for (int i = snake.num - 1; i > 0; i--)
 	{
 		snake.xy[i].x = snake.xy[i - 1].x;
 		snake.xy[i].y = snake.xy[i - 1].y;
@@ -69,7 +82,7 @@ int moveSnake()
 	case left:
 		snake.xy[0].x -= 10;
 		break;
-	case down:	
+	case down:
 		snake.xy[0].y += 10;
 		break;
 	case up:
@@ -124,17 +137,132 @@ void keyDown()
 	}
 }
 
+void myFood()
+{
+	food.foodxy.x = rand() % 80 * 10;
+	food.foodxy.y = rand() % 60 * 10;
+	food.flag = 1;
+	for (int i = 0; i < snake.num; i++)
+	{
+		if (food.foodxy.x == snake.xy[i].x && food.foodxy.y == snake.xy[i].y)
+		{
+			food.foodxy.x = rand() % 80 * 10;
+			food.foodxy.y = rand() % 60 * 10;
+		}
+	}
+}
+
+void drawFood()
+{
+	fillrectangle(food.foodxy.x, food.foodxy.y, food.foodxy.x + 10, food.foodxy.y + 10);
+	setfillcolor(RED);
+}
+
+void eatFood()
+{
+	if (snake.xy[0].x == food.foodxy.x && snake.xy[0].y == food.foodxy.y)
+	{
+		snake.num++;
+		food.eatGrade += 1;
+		food.flag = 0;
+	}
+}
+
+void myBoom()
+{
+	boom.Boomxy.x = rand() % 80 * 10;
+	boom.Boomxy.y = rand() % 60 * 10;
+	boom.flag = 1;
+	for (int i = 0; i < snake.num; i++)
+	{
+		if (boom.Boomxy.x == snake.xy[i].x && boom.Boomxy.y == snake.xy[i].y)
+		{
+			boom.Boomxy.x = rand() % 80 * 10;
+			boom.Boomxy.y = rand() % 60 * 10;
+		}
+	}
+}
+
+void eatBoom()
+{
+	if (snake.xy[0].x == boom.Boomxy.x && snake.xy[0].y == boom.Boomxy.y)
+	{
+		boom.flag = 0;
+		food.eatGrade -= 5;
+		snake.num = snake.num / 2;
+	}
+}
+
+void drawBoom()
+{
+	fillrectangle(boom.Boomxy.x, boom.Boomxy.y, boom.Boomxy.x + 10, boom.Boomxy.y + 10);
+	setfillcolor(BLACK);
+}
+
+void myGrass()
+{
+	grass.grassxy.x = rand() % 80 * 10;
+	grass.grassxy.y = rand() % 60 * 10;
+	grass.flag = 1;
+	for (int i = 0; i < snake.num; i++)
+	{
+		if (grass.grassxy.x == snake.xy[i].x && grass.grassxy.y == snake.xy[i].y)
+		{
+			grass.grassxy.x = rand() % 80 * 10;
+			grass.grassxy.y = rand() % 60 * 10;
+		}
+	}
+}
+
+void drawGrass()
+{
+	fillrectangle(grass.grassxy.x, grass.grassxy.y, grass.grassxy.x + 10, grass.grassxy.y + 10);
+	setfillcolor(GREEN);
+}
+
+void eatGrass()
+{
+	if (snake.xy[0].x == grass.grassxy.x && snake.xy[0].y == grass.grassxy.y)
+	{
+		grass.flag = 0;
+		food.eatGrade -= 2;
+		snake.num = snake.num - 2;
+	}
+}
+
 void main()
 {
-	initgraph(800,600);
+	srand((unsigned int)time(NULL));
+	initgraph(800, 600);
+	rectangle(0, 30, 800, 600);
+	setlinecolor(BLACK);
+	setbkcolor(WHITE);
 	initSnake();
 	drawSnake();
 	while (1)
 	{
 		cleardevice();
+		drawFood();
+		drawBoom();
+		drawGrass();
+		if (boom.flag == 0)
+		{
+			myBoom();
+		}
+		if (food.flag == 0)
+		{
+			myFood();
+		}
+		if (grass.flag == 0)
+		{
+			myGrass();
+		}
+		eatFood();
+		eatBoom();
+		eatGrass();
 		drawSnake();
 		moveSnake();
-		Sleep(300);
+		Sleep(100);
 		while (_kbhit())
 		{
 			keyDown();
